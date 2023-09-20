8e0642752c0e15b9e75f31e1841bbd80610c2bd3
randyfortier <randy.fortier@uoit.ca>
2023-09-20 14:07:03 -0400

    Added 03a examples for section 006

--- section006/03a_loops.py
+++ section006/03a_loops.py
@@ -1,1 +1,40 @@
 # 03a - Loops
+
+# infinite loop (x is always less than 10)
+
+# x = 0
+# while x < 10:
+#     print(f'{x} is small')
+#     x -= 1
+
+print('While loop:')
+x = 0
+while x < 10:
+    print(f'{x} is small')
+    x += 1
+
+print('For loop')
+for x in range(0, 10, 1): # [0,1,2,3,4,5,6,7,8,9]
+    print(f'{x} is small')
+
+print('For loop (non-one increment):')
+for x in range(15, 99, 7):
+    print(f'{x} is small')
+
+print('For loop (backwards):')
+for x in range(100, 87, -1):
+    print(f'{x} is small')
+
+
+# Coding Exercise 03a.1
+
+import math
+
+x = 1.0
+num_reps = 100
+estimate = 0.0
+for n in range(0, num_reps):
+    term = x ** n / math.factorial(n)
+    estimate += term 
+
+print(f'{estimate = }')
