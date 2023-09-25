fe1573786e75dfe801769c6d462fba9c939f2a87
randyfortier <randy.fortier@uoit.ca>
2023-09-25 10:53:54 -0400

    Added 03b examples for section 001

--- section001/03b_strings_lists.py
+++ section001/03b_strings_lists.py
@@ -1,2 +1,42 @@
 # 03b - Strings and Lists
 
+# Coding Exercise 03b.1
+
+full_name = 'Carla Rodriguez'
+first = ''
+last = ''
+first_name_done = False
+
+for char in full_name:
+    # three cases:
+    # 1. encounter the space (first name is done, first -> last)
+    # 2. encounter a non-space -> first
+    # 3. encounter a non-space -> last
+    if char == ' ':
+        first_name_done = True 
+    
+    elif not first_name_done:
+        # character is part of the first name
+        # first = first + char
+        first += char 
+
+    else:
+        # character is part of the last name 
+        last += char 
+
+print(f'"{first}","{last}"')
+
+# lists
+
+nums = [0,2,4,6,4,8]
+nums.remove(4) # removes the first 4
+print(f'{nums = }')
+
+# Coding Exercise 03b.2
+
+marks = [9.0, 10.0]
+mark_sum = 0.0
+for mark in marks:
+    mark_sum += mark 
+print(f'{mark_sum / len(marks) = }')
+print(f'{sum(marks) / len(marks) = }')
