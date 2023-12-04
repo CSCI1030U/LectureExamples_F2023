e8fb3c973e49d0a5c7d85aa8c7b58dc72549bbfe
randyfortier <randy.fortier@uoit.ca>
2023-12-04 12:21:42 -0500

    Added 11 examples for section 001

--- section001/11_ai.py
+++ section001/11_ai.py
@@ -1,2 +1,41 @@
 # 11 - Artificial Intelligence
 
+import numpy as np
+
+# basic forward propagation for a single neuron layer
+inputs = [0.1, 0.9, 0.5, 0.8]
+weights = [
+    [0.2, -0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87],
+]
+biases = [0.2, 0.8, 0.1]
+
+layer_outputs = np.dot(weights, inputs) + biases 
+print(f'{layer_outputs = }')
+
+# class implementation
+class NeuronLayer:
+    def __init__(self, weights, biases):
+        self._weights = weights 
+        self._biases = biases 
+    
+    def forward(self, inputs):
+        return self.relu(np.dot(self._weights, inputs) + self._biases)
+    
+    def relu(self, weighted_sums):
+        # results = []
+        # for weighted_sum in weighted_sums:
+        #     value = 0.2 * weighted_sum + 0.1
+        #     results.append(max(0.0, value))
+        # return results 
+        # return np.maximum(0.0, weighted_sums)
+        # scaled rectified linear (relu)
+        return np.maximum(0.0, 0.2 * weighted_sums + 0.1)
+
+hidden_layer1 = NeuronLayer(weights = [
+    [0.2, -0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87],
+], biases = [0.2, 0.8, 0.1])
+print(f'{hidden_layer1.forward(inputs = [0.1, 0.9, 0.5, 0.8])}')
--- section017/11_ai.py
+++ section017/11_ai.py
@@ -1,2 +1,34 @@
 # 11 - Artificial Intelligence
 
+import numpy as np
+
+# basic forward prop calculation
+inputs = [0.2, 2.0, 1.3, 2.1]
+weights = [
+    [0.2, 0.8, -0.5, 1.0], 
+    [0.5, -0.91, 0.17, 0.87], 
+    [-0.26, -0.27, 0.17, 0.87],
+]
+biases = [1.2, 1.3, 0.5]
+
+layer_outputs = np.dot(weights, inputs) + biases
+
+print(f'{layer_outputs = }')
+
+class NeuronLayer:
+    def __init__(self, weights, biases):
+        self._weights = weights 
+        self._biases = biases 
+
+    def forward(self, inputs):
+        return self.relu(np.dot(self._weights, inputs) + self._biases)
+
+    def relu(self, dot_products):
+        # results = []
+        # for dot_product in dot_products:
+        #     results.append(max(0.0, dot_product))
+        # return results
+        return np.maximum(0.0, 0.2 * dot_products + 0.1)
+
+layer1 = NeuronLayer(weights = [[0.2, 0.8, -0.5, 1.0], [0.1, -0.91, 0.17, -0.87], [-0.26, -0.27, 0.17, 0.87],], biases = [1.2, 1.3, 0.5])
+print(f'{layer1.forward(inputs = [0.2, 2.0, 1.3, 2.1]) = }')
